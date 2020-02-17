a70fc8e5f36afb79795e1896593b638ac99ee64c
phuong144 <patrickphuong07@gmail.com>
2020-02-17 00:43:03 -0800

    Set up file hierarchy, basic web server, and user signup api

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json()); // Converts data body into JSON format (originally bodyParser)
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+    .then(() => console.log("MongoDB connection established successfully!"))
+    .catch(error => console.log(error));
+
+// Middleware
+
+const { User } = require('./models/user');
+
+// Put this into routes folder in the future
+app.post('/api/user/signup', (req, res) => {
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+    }).save((err, response) =>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(response);
+    })
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`Server running on ${port}`);
+});
+
+
