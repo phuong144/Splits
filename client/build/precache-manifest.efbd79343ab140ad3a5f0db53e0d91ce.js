c6680000278a871fb40c827e8a97b01ccb317db6
phuong144 <patrickphuong07@gmail.com>
2020-07-19 22:37:06 -0700

    build

---
+++ client/build/precache-manifest.efbd79343ab140ad3a5f0db53e0d91ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49b0d7328e288e32befb40a180ec72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f638fdcee13a025a774f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3644d3a1473a6e637161",
+    "url": "/static/css/main.099d8b7c.chunk.css"
+  },
+  {
+    "revision": "f638fdcee13a025a774f",
+    "url": "/static/js/2.7cfb3814.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7cfb3814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3644d3a1473a6e637161",
+    "url": "/static/js/main.dde93120.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
