179a25d3f8eee23eff9f3cfb7d39178fe0bd79ae
phuong144 <patrickphuong07@gmail.com>
2020-12-18 01:00:28 -0800

    Leave build in client folder

---
+++ client/build/precache-manifest.a3cad49969cfa30878aa59f6e0ee1a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52c3f85b9d5c79aa45e8fe3559acd1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f223c5bdb5089a60466",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f76e93516eb3e887c6e8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f223c5bdb5089a60466",
+    "url": "/static/js/2.8c13d8d7.chunk.js"
+  },
+  {
+    "revision": "c622eea1db9789ca277337ee335a0d52",
+    "url": "/static/js/2.8c13d8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76e93516eb3e887c6e8",
+    "url": "/static/js/main.ffcba912.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
